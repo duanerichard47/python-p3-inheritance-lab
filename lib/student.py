283db1c32d1e7ea9d37d705cb2e3000ed22fc474
Duane <duanerichard47@hotmail.com>
2023-02-27 20:48:28 -0500

    complete

--- lib/student.py
+++ lib/student.py
@@ -3,13 +3,10 @@
 from user import User
 
 class Student(User):
-   
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge = []
  
-    def learn(self,first_name,last_name, knowledge,add_knowledge):
-        super().__init__(self,first_name,last_name)
-        self.knowledge = knowledge
-        knowledge = []
-        self.add_knowledge = add_knowledge
-
-        knowledge.append(add_knowledge)
+    def learn(self,add_knowledge):
+        self.knowledge.append(add_knowledge)
         
